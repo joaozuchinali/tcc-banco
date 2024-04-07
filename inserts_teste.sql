ecf77f009af7c4f47a2104ef624e68704a87e9df
João Vitor Zucchinali Ghislandi <joaozuchinalig@gmail.com>
2024-04-07 14:38:41 -0300

    nova modelagem do banco de dados

--- inserts_teste.sql
+++ inserts_teste.sql
@@ -1,3 +1,3 @@
-INSERT INTO equipe (idequipe) VALUES (1)
+INSERT INTO equipe (idequipe) VALUES (1);
 
 INSERT INTO projeto (nome, identificador, codigo, idequipe) VALUES ('teste', 'AAABBBCCC', 123456, 1);
